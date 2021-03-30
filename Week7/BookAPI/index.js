ea00bfb974154b9d2d1000d6e11e1a872ad867e2
Shanta-Dutta <74420422+Shanta-Dutta@users.noreply.github.com>
2021-03-30 17:25:35 -0400

    Add files via upload

---
+++ Week7/BookAPI/index.js
@@ -0,0 +1,18 @@
+// function getbooks(){
+//     document.getElementById('output').innerHTML="";
+//     fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value).then(a => a.json()).then(response => {
+//         for(var i=0; i<10; i++){
+//             document.getElementById("output").innerHTML+="<h2>"+response.docs[i].title+"</h2>"+response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br>";
+//         }
+//     })
+// }
+
+function getbooks(){
+    document.getElementById('output').innerHTML="";
+    fetch("http://openlibrary.org/search.json?q="+document.getElementById("input").value).then(a => a.json()).then(response => {
+        for(var i=0; i<10; i++){
+            let book = "<div class='space'><h3>"+"Title : "+response.docs[i].title+"</h3>"+"<h5>"+"Author Name: "+response.docs[i].author_name[0]+"</h5><br><br><img src='http://covers.openlibrary.org/b/isbn/"+response.docs[i].isbn[0]+"-M.jpg'><br><br></div><br><br>";
+            document.getElementById("output").innerHTML += book;
+        }
+    })
+}
